0846a412eda27d4d50d2456c5a713f69612df2e3
Bezzubov Artem <66363533+Springfields1996@users.noreply.github.com>
2020-09-09 16:37:09 +0300

    Updates

---
+++ precache-manifest.abaeaa03b4a33957367bca3bf262c4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f011e50a839a54e6856725c2d79b61e",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "9c419e9cd72e61e9ade9",
+    "url": "/goit-react-hw-03-phonebook/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "9c419e9cd72e61e9ade9",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.ef8333ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.ef8333ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c38f324114075189a9",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.57044c1d.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
